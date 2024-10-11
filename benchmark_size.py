f855dea113b9db02e9e86e56707363e35d89d335
Nico <nico.voss@physik.uni-regensburg.de>
2024-10-11 13:47:24 -0400

    Update benchmark_size.py

--- benchmark_size.py
+++ benchmark_size.py
@@ -5,52 +5,55 @@ import socket
 import qcd_ml_accel_dirac
 import qcd_ml
 
-num_threads = torch.get_num_threads()
-print("running on host", socket.gethostname())
-print(f'Machine has {num_threads} threads')
+def now_try_as_a_test():
+    num_threads = torch.get_num_threads()
+    print("running on host", socket.gethostname())
+    print(f'Machine has {num_threads} threads')
 
-sizes = [[4,4,4,8],[8,4,4,8],[8,8,4,8],[8,8,8,8],[8,8,8,16],[16,8,8,16],[16,16,8,16],[16,16,16,16]]
+    sizes = [[4,4,4,8],[8,4,4,8],[8,8,4,8],[8,8,8,8],[8,8,8,16],[16,8,8,16],[16,16,8,16],[16,16,16,16]]
 
-mass = -0.5
-csw = 1.0
-tn = 4
+    mass = -0.5
+    csw = 1.0
+    tn = 4
 
-for si in sizes:
+    for si in sizes:
 
-    print(f"size is {si[0]}x{si[1]}x{si[2]}x{si[3]}")
+        print(f"size is {si[0]}x{si[1]}x{si[2]}x{si[3]}")
 
-    U = torch.randn([4]+si+[3,3],dtype=torch.cdouble)
-    v = torch.randn(si+[4,3],dtype=torch.cdouble)
+        U = torch.randn([4]+si+[3,3],dtype=torch.cdouble)
+        v = torch.randn(si+[4,3],dtype=torch.cdouble)
 
-    t0 = benchmark.Timer(
-        stmt='dw_py(v)',
-        setup='from qcd_ml.qcd.dirac import dirac_wilson; dw_py = dirac_wilson(U,m)',
-        globals={'U': U, 'v': v, 'm': mass},
-        num_threads=tn
-    )
+        t0 = benchmark.Timer(
+            stmt='dw_py(v)',
+            setup='from qcd_ml.qcd.dirac import dirac_wilson; dw_py = dirac_wilson(U,m)',
+            globals={'U': U, 'v': v, 'm': mass},
+            num_threads=tn
+        )
 
-    t1 = benchmark.Timer(
-        stmt='dw_cpp(v)',
-        setup='from qcd_ml_accel_dirac import dirac_wilson; dw_cpp = dirac_wilson(U,m)',
-        globals={'U': U, 'v': v, 'm': mass},
-        num_threads=tn
-    )
+        t1 = benchmark.Timer(
+            stmt='dw_cpp(v)',
+            setup='from qcd_ml_accel_dirac import dirac_wilson; dw_cpp = dirac_wilson(U,m)',
+            globals={'U': U, 'v': v, 'm': mass},
+            num_threads=tn
+        )
 
-    t2 = benchmark.Timer(
-        stmt='dwc_py(v)',
-        setup='from qcd_ml.qcd.dirac import dirac_wilson_clover; dwc_py = dirac_wilson_clover(U,m,c)',
-        globals={'U': U, 'v': v, 'm': mass, 'c': csw},
-        num_threads=tn
-    )
+        t2 = benchmark.Timer(
+            stmt='dwc_py(v)',
+            setup='from qcd_ml.qcd.dirac import dirac_wilson_clover; dwc_py = dirac_wilson_clover(U,m,c)',
+            globals={'U': U, 'v': v, 'm': mass, 'c': csw},
+            num_threads=tn
+        )
 
-    t3 = benchmark.Timer(
-        stmt='dwc_cpp(v)',
-        setup='from qcd_ml_accel_dirac import dirac_wilson_clover; dwc_cpp = dirac_wilson_clover(U,m,c)',
-        globals={'U': U, 'v': v, 'm': mass, 'c': csw},
-        num_threads=tn
-    )
+        t3 = benchmark.Timer(
+            stmt='dwc_cpp(v)',
+            setup='from qcd_ml_accel_dirac import dirac_wilson_clover; dwc_cpp = dirac_wilson_clover(U,m,c)',
+            globals={'U': U, 'v': v, 'm': mass, 'c': csw},
+            num_threads=tn
+        )
 
-    print(t0.timeit(30))
-    print(t1.timeit(30))
-    print(t2.timeit(30))
-    print(t3.timeit(30))
+        print(t0.timeit(30))
+        print(t1.timeit(30))
+        print(t2.timeit(30))
+        print(t3.timeit(30))
+    
+    assert True
