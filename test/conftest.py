f8577164e3a3cdf55f5d76389d84a85832324d33
Nico <nico.voss@physik.uni-regensburg.de>
2024-10-14 17:59:59 -0400

    add test for correct plaquette

--- test/conftest.py
+++ test/conftest.py
@@ -10,15 +10,19 @@ import os
 def config_1500():
     return torch.tensor(np.load(os.path.join(os.path.dirname(__file__), "assets","1500.config.npy")))
 
-
 @pytest.fixture 
 def psi_test():
     return torch.tensor(np.load(os.path.join(os.path.dirname(__file__), "assets","psi_test.npy")))
 
-
 @pytest.fixture 
 def psi_Dw1500_m0p5_psitest():
     return torch.tensor(np.load(os.path.join(os.path.dirname(__file__), "assets","psi_Dw1500_m0p5_psitest.npy")))
+
 @pytest.fixture 
 def psi_Dwc1500_m0p5_psitest():
     return torch.tensor(np.load(os.path.join(os.path.dirname(__file__), "assets","psi_Dwc1500_m0p5_psitest.npy")))
+
+@pytest.fixture 
+def config_1500_gtrans_1200mu0():
+    return torch.tensor(np.load(os.path.join(os.path.dirname(__file__), "assets","1500_gtrans_1200mu0.npy")))
+
