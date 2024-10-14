41fed50c91355dd766a1269c6a82793244f9006c
Nico <nico.voss@physik.uni-regensburg.de>
2024-10-14 14:42:17 -0400

    Update test_06_bench_solver.py

--- test/test_06_bench_solver.py
+++ test/test_06_bench_solver.py
@@ -15,27 +15,28 @@ def test_solver_with_python_vs_cpp_wilson():
 
     #v = torch.randn([8,8,8,16,4,3], dtype=torch.cdouble)
 
-    t0 = benchmark.Timer(
-        stmt='qcd_ml.util.solver.GMRES(dwc_py, v, v, eps=1e-4, inner_iter=25, maxiter=400)',
-        setup='import torch; import qcd_ml; '
-            + 'U = torch.randn([4,8,8,8,16,3,3], dtype=torch.cdouble); '
-            + 'v = torch.randn([8,8,8,16,4,3], dtype=torch.cdouble); '
-            + 'dwc_py = qcd_ml.qcd.dirac.dirac_wilson_clover(U, -0.61, 1)',
-        #globals={'U': U, 'v': v},
-        num_threads=num_threads
-    )
-    t1 = benchmark.Timer(
-        stmt='qcd_ml.util.solver.GMRES(dwc_cpp, v, v, eps=1e-4, inner_iter=25, maxiter=400)',
-        setup='import torch; import qcd_ml; import qcd_ml_accel_dirac; '
-            + 'U = torch.randn([4,8,8,8,16,3,3], dtype=torch.cdouble); '
-            + 'v = torch.randn([8,8,8,16,4,3], dtype=torch.cdouble); '
-            + 'dwc_cpp = qcd_ml_accel_dirac.dirac_wilson_clover(U, -0.61, 1)',
-        #globals={'U': U, 'v': v},
-        num_threads=num_threads
-    )
+    for tn in range(1,num_threads+1):
+        t0 = benchmark.Timer(
+            stmt='qcd_ml.util.solver.GMRES(dwc_py, v, v, eps=1e-4, inner_iter=25, maxiter=400)',
+            setup='import torch; import qcd_ml; '
+                + 'U = torch.randn([4,8,8,8,16,3,3], dtype=torch.cdouble); '
+                + 'v = torch.randn([8,8,8,16,4,3], dtype=torch.cdouble); '
+                + 'dwc_py = qcd_ml.qcd.dirac.dirac_wilson_clover(U, -0.61, 1)',
+            #globals={'U': U, 'v': v},
+            num_threads=tn
+        )
+        t1 = benchmark.Timer(
+            stmt='qcd_ml.util.solver.GMRES(dwc_cpp, v, v, eps=1e-4, inner_iter=25, maxiter=400)',
+            setup='import torch; import qcd_ml; import qcd_ml_accel_dirac; '
+                + 'U = torch.randn([4,8,8,8,16,3,3], dtype=torch.cdouble); '
+                + 'v = torch.randn([8,8,8,16,4,3], dtype=torch.cdouble); '
+                + 'dwc_cpp = qcd_ml_accel_dirac.dirac_wilson_clover(U, -0.61, 1)',
+            #globals={'U': U, 'v': v},
+            num_threads=tn
+        )
 
-    print(t0.timeit(3))
-    print(t1.timeit(3))
+        print(t0.timeit(3))
+        print(t1.timeit(3))
 
     assert True
 
