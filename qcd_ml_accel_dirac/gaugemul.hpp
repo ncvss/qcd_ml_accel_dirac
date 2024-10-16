ee9d379e197697c2adfa7856893c19ed6dc72a89
Nico <nico.voss@physik.uni-regensburg.de>
2024-10-16 14:34:24 -0400

    move function definitions to cpp files

--- qcd_ml_accel_dirac/gaugemul.hpp
+++ qcd_ml_accel_dirac/gaugemul.hpp
@@ -1,12 +1,13 @@
 #include <torch/extension.h>
 #include <vector>
 
-#ifndef _OPENMP
-#define _OPENMP
-#endif
-#include <ATen/ParallelOpenMP.h>
-#include <omp.h>
+// #ifndef _OPENMP
+// #define _OPENMP
+// #endif
+// #include <ATen/ParallelOpenMP.h>
+// #include <omp.h>
 
+// #include "indexfunc.hpp"
 
 // file for the function shift_gaugemul
 
@@ -16,139 +17,16 @@ namespace qcd_ml_accel_dirac{
 // Their positions on the grid in each dimension are shifted by
 // the corresponding component of u2shifts and uvshifts respectively.
 
+/**
+ * @brief multiply a gauge field with a gauge or vector field, with position shifts
+ * 
+ * @param U2 a gauge field, (Lx,Ly,Lz,Lt,3,3)-tensor
+ * @param Uv a gauge field or vector field, (Lx,Ly,Lz,Lt,n,3)-tensor. n=3 corresponds to gauge, otherwise vector field.
+ * @param u2shifts the positional shifts of U2 in each lattice axis
+ * @param uvshifts the positional shifts of U2 in each lattice axis
+ * @return at::Tensor the product
+ */
 at::Tensor shift_gaugemul_p_cpu (const at::Tensor& U2, const at::Tensor& Uv,
-                                std::vector<int64_t> u2shifts, std::vector<int64_t> uvshifts){
-
-    // check that both tensors have the correct shape for a gauge or vector field tensor
-    TORCH_CHECK(Uv.dim() == 6);
-    TORCH_CHECK(U2.dim() == 6);
-    TORCH_CHECK(Uv.size(0) == U2.size(0));
-    TORCH_CHECK(Uv.size(1) == U2.size(1));
-    TORCH_CHECK(Uv.size(2) == U2.size(2));
-    TORCH_CHECK(Uv.size(3) == U2.size(3));
-    TORCH_CHECK(U2.size(4) == 3);
-    TORCH_CHECK(U2.size(5) == 3);
-    TORCH_CHECK(Uv.size(5) == 3);
-
-    // check that the shift vector length is the number of space-time dimensions
-    TORCH_CHECK(u2shifts.size() == 4);
-    TORCH_CHECK(uvshifts.size() == 4);
-
-    // check that both tensors are complex double
-    TORCH_CHECK(U2.dtype() == at::kComplexDouble);
-    TORCH_CHECK(Uv.dtype() == at::kComplexDouble);
-
-    TORCH_INTERNAL_ASSERT(U2.device().type() == at::DeviceType::CPU);
-    TORCH_INTERNAL_ASSERT(Uv.device().type() == at::DeviceType::CPU);
-
-    at::Tensor Uv_contig = Uv.contiguous();
-    at::Tensor U2_contig = U2.contiguous();
-
-    // sizes of the different axes
-    int64_t uvsize [6];
-    int64_t u2size [6];
-    for (int64_t sj = 0; sj < 6; sj++){
-        uvsize[sj] = Uv_contig.size(sj);
-        u2size[sj] = U2_contig.size(sj);
-    }
-
-    // strides of the memory blocks
-    int64_t uvstride [6];
-    int64_t u2stride [6];
-    uvstride[5] = 1;
-    u2stride[5] = 1;
-    for (int64_t sj = 4; sj >= 0; sj--){
-        uvstride [sj] = uvstride[sj+1] * uvsize[sj+1];
-        u2stride [sj] = u2stride[sj+1] * u2size[sj+1];
-    }
-
-    // if shifts are negative, they have to be changed to the positive equivalent
-    // this is because the modulo has undefined behavior for negative shifts
-    for (int64_t sd = 0; sd < 4; sd++){
-        if (uvshifts[sd] < 0){
-            uvshifts[sd] += uvsize[sd];
-        }
-        if (u2shifts[sd] < 0){
-            u2shifts[sd] += u2size[sd];
-        }
-    }
-
-    at::Tensor result = torch::zeros(uvsize, Uv.options());
-
-    const c10::complex<double>* Uv_ptr = Uv_contig.data_ptr<c10::complex<double>>();
-    const c10::complex<double>* U2_ptr = U2_contig.data_ptr<c10::complex<double>>();
-    c10::complex<double>* res_ptr = result.data_ptr<c10::complex<double>>();
-
-
-    // different multiplication if Uv is a gauge or vector field
-    // the gauge field is identified by its axis 4 having length 3
-    // if Uv is a gauge field, this is a SU3 matrix product, and result is a gauge field
-    // if Uv is a vector, this is a gauge transform, which acts on the last dimension of Uv, and result is a vector
-    if (uvsize[4] == 3){
-        // simple matrix multiplication, where the elements are accessed with shifts
-
-        // parallelisation
-        at::parallel_for(0, uvsize[0], 1, [&](int64_t start, int64_t end){
-        for (int64_t x = start; x < end; x++){
-            for (int64_t y = 0; y < uvsize[1]; y++){
-                for (int64_t z = 0; z < uvsize[2]; z++){
-                    for (int64_t t = 0; t < uvsize[3]; t++){
-                        for (int64_t g = 0; g < 3; g++){
-                            for (int64_t h = 0; h < 3; h++){
-                                for (int64_t i = 0; i < 3; i++){
-                                    res_ptr[ptridx6(x,y,z,t,g,h,uvstride)]
-                                    +=  U2_ptr[ptridx6((x + u2shifts[0])%u2size[0],
-                                                       (y + u2shifts[1])%u2size[1],
-                                                       (z + u2shifts[2])%u2size[2],
-                                                       (t + u2shifts[3])%u2size[3],
-                                                        g, i, u2stride)]
-                                       *Uv_ptr[ptridx6((x + uvshifts[0])%uvsize[0],
-                                                       (y + uvshifts[1])%uvsize[1],
-                                                       (z + uvshifts[2])%uvsize[2],
-                                                       (t + uvshifts[3])%uvsize[3],
-                                                        i, h, uvstride)];
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        });
-    } else {
-        // matrix multiplication w.r.t. the last indices of both tensors
-
-        // parallelisation
-        at::parallel_for(0, uvsize[0], 1, [&](int64_t start, int64_t end){
-        for (int64_t x = start; x < end; x++){
-            for (int64_t y = 0; y < uvsize[1]; y++){
-                for (int64_t z = 0; z < uvsize[2]; z++){
-                    for (int64_t t = 0; t < uvsize[3]; t++){
-                        for (int64_t g = 0; g < 3; g++){
-                            for (int64_t h = 0; h < uvsize[4]; h++){
-                                for (int64_t i = 0; i < 3; i++){
-                                    res_ptr[ptridx6(x,y,z,t,g,h,uvstride)]
-                                    +=  U2_ptr[ptridx6((x + u2shifts[0])%u2size[0],
-                                                       (y + u2shifts[1])%u2size[1],
-                                                       (z + u2shifts[2])%u2size[2],
-                                                       (t + u2shifts[3])%u2size[3],
-                                                        g, i, u2stride)]
-                                       *Uv_ptr[ptridx6((x + uvshifts[0])%uvsize[0],
-                                                       (y + uvshifts[1])%uvsize[1],
-                                                       (z + uvshifts[2])%uvsize[2],
-                                                       (t + uvshifts[3])%uvsize[3],
-                                                        h, i, uvstride)];
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        });
-    }
-
-    return result;
-}
+                                std::vector<int64_t> u2shifts, std::vector<int64_t> uvshifts);
 
 }
--- qcd_ml_accel_dirac/plaq.hpp
+++ qcd_ml_accel_dirac/plaq.hpp
@@ -1,115 +1,24 @@
 #include <torch/extension.h>
 #include <vector>
 
-#ifndef _OPENMP
-#define _OPENMP
-#endif
-#include <ATen/ParallelOpenMP.h>
-#include <omp.h>
+// #ifndef _OPENMP
+// #define _OPENMP
+// #endif
+// #include <ATen/ParallelOpenMP.h>
+// #include <omp.h>
 
 // file for the plaquette action
 
 namespace qcd_ml_accel_dirac{
 
-double plaq_action_cpu (const at::Tensor& U, double g){
-    
-    // check that U has the correct shape for a gauge tensor
-    TORCH_CHECK(U.dim() == 7);
-    TORCH_CHECK(U.size(5) == 3);
-    TORCH_CHECK(U.size(6) == 3);
-
-    // check that U is complex double
-    TORCH_CHECK(U.dtype() == at::kComplexDouble);
-
-    TORCH_INTERNAL_ASSERT(U.device().type() == at::DeviceType::CPU);
-
-    at::Tensor U_contig = U.contiguous();
-
-    // sizes of the different axes
-    int64_t u_size [7];
-    for (int64_t sj = 0; sj < 7; sj++){
-        u_size[sj] = U_contig.size(sj);
-    }
-    // strides of the memory blocks
-    int64_t u_stride [7];
-    u_stride[6] = 1;
-    for (int64_t sj = 5; sj >= 0; sj--){
-        u_stride [sj] = u_stride[sj+1] * u_size[sj+1];
-    }
-
-    // sizes and strides for a gauge tensor for a specific direction
-    int64_t um_size [6];
-    for (int64_t si = 0; si < 6; si++){
-        um_size[si] = u_size[si+1];
-    }
-    int64_t um_stride [6];
-    um_stride[5] = 1;
-    for (int64_t sj = 4; sj >= 0; sj--){
-        um_stride [sj] = um_stride[sj+1] * um_size[sj+1];
-    }
-
-    const c10::complex<double>* U_ptr = U_contig.data_ptr<c10::complex<double>>();
-
-    // refer to the python version for a more intuitive computation
-
-    // lookup tables for shifts in direction mu (basically an identity matrix)
-    std::vector<std::vector<int64_t>> shvec = {{1,0,0,0},{0,1,0,0},{0,0,1,0},{0,0,0,1}};
-
-    // lookup tables for the possible tuples of mu and nu
-    int64_t nus [6] = {1,2,2,3,3,3};
-    int64_t mus [6] = {0,0,1,0,1,2};
-
-    double result = 0.0;
-    
-    // parallel loop over the possible tuples (mu,nu)
-    // not good to parallelize, as a matrix multiplication happens inside
-    // as per the docs, parallelization should not contain tensor operations
-    //at::parallel_reduce(0, 6, 1, 0, [&](int64_t start, int64_t end){
-    for (int64_t im = 0; im < 6; im++){
-        //for (int64_t nu = 0; nu < 4; nu++){
-        //for (int64_t mu = 0; mu < nu; mu++){
-            // only compute the matrix product of the first 3 matrices
-            // the fourth one is unneeded, as we only take the trace
-            // also, we reverse the order to only call the adjoint once
-            at::Tensor Umn_prel = shift_gaugemul_p_cpu(U[nus[im]],
-                                                       at::adjoint(shift_gaugemul_p_cpu(U[nus[im]], U[mus[im]], {0,0,0,0}, shvec[nus[im]])),
-                                                       shvec[mus[im]], {0,0,0,0});
-            at::Tensor Umnp_contig = Umn_prel.contiguous();
-            const c10::complex<double>* Umnp_ptr = Umnp_contig.data_ptr<c10::complex<double>>();
-
-            double munu_result = at::parallel_reduce(0, u_size[1], 1, 0.0, [&](int64_t start, int64_t end, double identify){
-            double interm_result = identify;
-            for (int64_t x = start; x < end; x++){
-                for (int64_t y = 0; y < u_size[2]; y++){
-                    for (int64_t z = 0; z < u_size[3]; z++){
-                        for (int64_t t = 0; t < u_size[4]; t++){
-                            // sum over 3 contributions to the trace
-                            for (int64_t g = 0; g < 3; g++){
-                                // the contribution is the gth row of U_mu times the gth column of Umn_prel
-                                interm_result += std::real( 1.0
-                                                    -U_ptr[ptridx7(mus[im],x,y,z,t,g,0,u_stride)] * Umnp_ptr[ptridx6(x,y,z,t,0,g,um_stride)]
-                                                    -U_ptr[ptridx7(mus[im],x,y,z,t,g,1,u_stride)] * Umnp_ptr[ptridx6(x,y,z,t,1,g,um_stride)]
-                                                    -U_ptr[ptridx7(mus[im],x,y,z,t,g,2,u_stride)] * Umnp_ptr[ptridx6(x,y,z,t,2,g,um_stride)]
-                                                    );
-                            }
-                        }
-                    }
-                }
-            }
-            return interm_result;
-            }, //[&](double r1, double r2){return r1+r2;}
-            std::plus<double>{});
-        //}
-        //}
-        result += munu_result;
-    }
-    //}, [](double r1, double r2){return r1+r2;});
-    
-    double vol = u_size[1]*u_size[2]*u_size[3]*u_size[4];
-
-    return result *2/(g*g)/vol/6;
-}
-
+/**
+ * @brief compute the plaquette action
+ * 
+ * @param U the gauge field configuration, (4,Lx,Ly,Lz,Lt,3,3)-tensor
+ * @param g the gauge coupling constant
+ * @return double, the normalized action
+ */
+double plaq_action_cpu (const at::Tensor& U, double g);
 
 }
 
