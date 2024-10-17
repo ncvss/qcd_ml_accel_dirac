49dcfeb644747c48c8e44135f911e5d73fa9d71b
Nico <nico.voss@physik.uni-regensburg.de>
2024-10-17 14:29:18 -0400

    Update test_09_domain_wall.py

--- test/test_09_domain_wall.py
+++ test/test_09_domain_wall.py
@@ -9,7 +9,20 @@ try:
 
 
     def test_domain_wall():
-        pass
+        rng = g.random("test")
+        U = g.qcd.gauge.random(g.grid([8, 8, 8, 16], g.double), rng, scale=2.0)
+        grid = U[0].grid
+
+        mobius_params = {
+            "mass": 0.08,
+            "M5": 1.8,
+            "b": 1.5,
+            "c": 0.5,
+            "Ls": 8,
+            "boundary_phases": [1.0, 1.0, 1.0, 1.0],
+        }
+
+        qm = g.qcd.fermion.mobius(U, mobius_params)
 
 
 except ImportError:
