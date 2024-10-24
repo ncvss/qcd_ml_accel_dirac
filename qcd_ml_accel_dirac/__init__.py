34248e522c4f4dbac9ee3357f1a9d16f165f81bf
Nico <nico.voss@physik.uni-regensburg.de>
2024-10-24 09:23:21 -0400

    remove rearranged operators

--- qcd_ml_accel_dirac/__init__.py
+++ qcd_ml_accel_dirac/__init__.py
@@ -1,4 +1,4 @@
 import torch
 from . import _C
-from .dirac_class import dirac_wilson, dirac_wilson_clover, dirac_wilson_clover_r, dirac_wilson_clover_r2
+from .dirac_class import dirac_wilson, dirac_wilson_clover
 from .plaq_p import _plaq_action_py, plaquette_action
