d5d89fa21cbc24266d6ea740f6ecc4023862adc2
Nico <nico.voss@physik.uni-regensburg.de>
2024-10-24 12:32:08 -0400

    add tests with perf_counter_ns

--- test/test_08_bench_dirac.py
+++ test/test_08_bench_dirac.py
@@ -1,6 +1,8 @@
 import torch
 import torch.utils.benchmark as benchmark
 import socket
+import numpy as np
+import time
 
 import qcd_ml_accel_dirac
 import qcd_ml
@@ -86,3 +88,144 @@ def test_pytorch_timer_wilson_clover():
 
     assert torch.allclose(dwc(v),dwc_cpp(v))
 
+
+
+def test_perf_counter_wilson():
+    print()
+    n_measurements = 1000
+    n_warmup = 10
+
+    U = torch.randn([4,8,8,8,16,3,3],dtype=torch.cdouble)
+    v = torch.randn([8,8,8,16,4,3],dtype=torch.cdouble)
+    mass = -0.5
+
+    dw_py = qcd_ml.qcd.dirac.dirac_wilson(U,mass)
+    dw_cpp = qcd_ml_accel_dirac.dirac_wilson(U,mass)
+
+    dwv_py = dw_py(v)
+    dwv_cpp = dw_cpp(v)
+
+    for _ in range(n_warmup):
+        dwv_py = dw_py(v)
+        dwv_cpp = dw_cpp(v)
+
+
+    results_py = np.zeros(n_measurements)
+    results_cpp = np.zeros(n_measurements)
+    bias = np.zeros(n_measurements)
+
+    for i in range(n_measurements):
+        start = time.perf_counter_ns()
+        dwv_py = dw_py(v)
+        stop = time.perf_counter_ns()
+        results_py[i] = stop - start
+
+        start = time.perf_counter_ns()
+        dwv_cpp = dw_cpp(v)
+        stop = time.perf_counter_ns()
+        results_cpp[i] = stop - start
+
+        start = time.perf_counter_ns()
+        stop = time.perf_counter_ns()
+        bias[i] = stop - start
+
+
+    results_py_sorted = np.sort(results_py)[:(n_measurements // 5)]
+    results_cpp_sorted = np.sort(results_cpp)[:(n_measurements // 5)]
+
+
+    for lang_name,results_sorted in [["python",results_py_sorted], ["c++",results_cpp_sorted]]:
+        print(lang_name)
+        print(f"mean (top 20%): [ns] {np.mean(results_sorted): .2f}")
+        print(f"std (top 20%): [ns] {np.std(results_sorted): .2f}")
+        print(f"best : [ns] {results_sorted[0]}")
+        print(f"mean bias : [ns] {np.mean(bias)}")
+        print(f"std bias : [ns] {np.mean(bias)}")
+
+        data_size = v.element_size() * v.nelement() + U.element_size() * U.nelement()
+        data_size_MiB = data_size / 1024**2
+
+        print()
+        print(f"data : [MiB] {data_size_MiB: .3f}")
+
+        throughput = data_size / (np.mean(results_sorted) / 1000**3)
+        throughput_GiBs = throughput / 1024 ** 3
+        throughput_peak = data_size / (results_sorted[0] / 1000**3)
+        throughput_peak_GiBs = throughput_peak / 1024 ** 3
+
+        print(f"throughput : [GiB/s] {throughput_GiBs: .3f}")
+        print(f"peak thrpt. : [GiB/s] {throughput_peak_GiBs: .3f}")
+        print("-----")
+
+    assert torch.allclose(dwv_cpp,dwv_py)
+
+
+def test_perf_counter_wilson_clover():
+    print()
+    n_measurements = 1000
+    n_warmup = 10
+
+    U = torch.randn([4,8,8,8,16,3,3],dtype=torch.cdouble)
+    v = torch.randn([8,8,8,16,4,3],dtype=torch.cdouble)
+    mass = -0.5
+    csw = 1.0
+
+    dwc_py = qcd_ml.qcd.dirac.dirac_wilson_clover(U,mass,csw)
+    dwc_cpp = qcd_ml_accel_dirac.dirac_wilson_clover(U,mass,csw)
+
+    dwcv_py = dwc_py(v)
+    dwcv_cpp = dwc_cpp(v)
+
+    for _ in range(n_warmup):
+        dwcv_py = dwc_py(v)
+        dwcv_cpp = dwc_cpp(v)
+
+
+    results_py = np.zeros(n_measurements)
+    results_cpp = np.zeros(n_measurements)
+    bias = np.zeros(n_measurements)
+
+    for i in range(n_measurements):
+        start = time.perf_counter_ns()
+        dwcv_py = dwc_py(v)
+        stop = time.perf_counter_ns()
+        results_py[i] = stop - start
+
+        start = time.perf_counter_ns()
+        dwcv_cpp = dwc_cpp(v)
+        stop = time.perf_counter_ns()
+        results_cpp[i] = stop - start
+
+        start = time.perf_counter_ns()
+        stop = time.perf_counter_ns()
+        bias[i] = stop - start
+
+
+    results_py_sorted = np.sort(results_py)[:(n_measurements // 5)]
+    results_cpp_sorted = np.sort(results_cpp)[:(n_measurements // 5)]
+
+
+    for lang_name,results_sorted in [["python",results_py_sorted], ["c++",results_cpp_sorted]]:
+        print(lang_name)
+        print(f"mean (top 20%): [ns] {np.mean(results_sorted): .2f}")
+        print(f"std (top 20%): [ns] {np.std(results_sorted): .2f}")
+        print(f"best : [ns] {results_sorted[0]}")
+        print(f"mean bias : [ns] {np.mean(bias)}")
+        print(f"std bias : [ns] {np.mean(bias)}")
+
+        data_size = v.element_size() * v.nelement() + U.element_size() * U.nelement() * 10/4
+        data_size_MiB = data_size / 1024**2
+
+        print()
+        print(f"data : [MiB] {data_size_MiB: .3f}")
+
+        throughput = data_size / (np.mean(results_sorted) / 1000**3)
+        throughput_GiBs = throughput / 1024 ** 3
+        throughput_peak = data_size / (results_sorted[0] / 1000**3)
+        throughput_peak_GiBs = throughput_peak / 1024 ** 3
+
+        print(f"throughput : [GiB/s] {throughput_GiBs: .3f}")
+        print(f"peak thrpt. : [GiB/s] {throughput_peak_GiBs: .3f}")
+        print("-----")
+
+    assert torch.allclose(dwcv_cpp,dwcv_py)
