b0285eed891c5a09492ba82a2dd2961293604cae
Nico <nico.voss@physik.uni-regensburg.de>
2025-01-07 19:24:15 +0100

    add test for plot

--- qcd_ml_accel_dirac/csrc/dirac_simd.cpp
+++ qcd_ml_accel_dirac/csrc/dirac_simd.cpp
@@ -270,4 +270,220 @@ at::Tensor dw_call_lookup_256d_cpu (const at::Tensor& U_tensor, const at::Tensor
     return result_tensor;
 }
 
+
+// at::Tensor dw_call_lookup_256d_old_layout (const at::Tensor& U_tensor, const at::Tensor& v_tensor,
+//                                            const at::Tensor& hops_tensor, double mass){
+    
+//     // now the indices are the old layout: U[mu,x,y,z,t,g,gi] and v[x,y,z,t,s,g]
+//     // which means the entire code does not work anymore!
+//     // And there is no way to make it work
+//     TORCH_CHECK(v_tensor.dim() == 6);
+//     TORCH_CHECK(U_tensor.size(1) == v_tensor.size(0));
+//     TORCH_CHECK(U_tensor.size(2) == v_tensor.size(1));
+//     TORCH_CHECK(U_tensor.size(3) == v_tensor.size(2));
+//     TORCH_CHECK(U_tensor.size(4) == v_tensor.size(3));
+//     TORCH_CHECK(v_tensor.size(5) == 3);
+//     TORCH_CHECK(v_tensor.size(4) == 4);
+
+//     TORCH_CHECK(U_tensor.dtype() == at::kComplexDouble);
+//     TORCH_CHECK(v_tensor.dtype() == at::kComplexDouble);
+
+//     TORCH_CHECK(U_tensor.is_contiguous());
+//     TORCH_CHECK(v_tensor.is_contiguous());
+
+//     int vol = U_tensor.size(1) * U_tensor.size(2) * U_tensor.size(3) * U_tensor.size(4);
+    
+
+//     at::Tensor result_tensor = torch::empty(v_tensor.sizes(), v_tensor.options());
+
+//     // we create a pointer to the complex tensor, then typecast it to double*
+//     // this allows us to access the complex numbers as doubles in riri format
+//     const double* U = (double*)U_tensor.const_data_ptr<c10::complex<double>>();
+//     const double* v = (double*)v_tensor.const_data_ptr<c10::complex<double>>();
+//     const int* hops = hops_tensor.const_data_ptr<int>();
+//     double* result = (double*)result_tensor.mutable_data_ptr<c10::complex<double>>();
+
+//     // register for the mass prefactor
+//     __m256d massf_reg = _mm256_set1_pd(4.0 + mass);
+
+//     // register for the -1/2 prefactor
+//     __m256d m0p5_reg = _mm256_set1_pd(-0.5);
+
+
+// #pragma omp parallel for
+//     for (int t = 0; t < vol; t++){
+
+//         // mu loop partially unrolled
+//         // for (int mu = 0; mu < 4; mu++){
+//         int mu = 0;
+//         for (int g = 0; g < 3; g++){
+//             for (int s = 0; s < 4; s+=2){
+
+//                 // mass term is the first term in the result
+//                 __m256d incr = _mm256_loadu_pd(v+vixd(t,g,s));
+//                 incr = _mm256_mul_pd(incr,massf_reg);
+
+//                 // gamma_mu prefactor for the spins s and s+1
+//                 __m256d gamf2s = _mm256_loadu_pd(gamfd+gixd(mu,s));
+
+//                 for (int gi = 0; gi < 3; gi++){
+//                     // v hop in negative mu * gammma
+//                     __m256d v_Hmum_gam = _mm256_loadu_pd(v+vixd(hops[hixd(t,mu,0)],gi,gamx_pd[s]));
+//                     // because mu=0,1: swap the 2 numbers in the register
+//                     v_Hmum_gam = _mm256_permute4x64_pd(v_Hmum_gam,78);
+//                     // multiply the gamma prefactor for s and s+1
+//                     v_Hmum_gam = compl_vectorreg_pointwise_mul(gamf2s,v_Hmum_gam);
+
+//                     // v hop in negative mu
+//                     __m256d v_Hmum = _mm256_loadu_pd(v+vixd(hops[hixd(t,mu,0)],gi,s));
+
+//                     // add those together
+//                     v_Hmum = _mm256_add_pd(v_Hmum, v_Hmum_gam);
+
+//                     // take Umu hop in negative mu, adjoint it, and multiply onto v sum
+//                     v_Hmum = compl_scalarmem_conj_vectorreg_mul(U+uixd(hops[hixd(t,mu,0)],mu,gi,g),v_Hmum);
+
+
+//                     // v hop in positive mu * gamma
+//                     __m256d v_Hmup_gam = _mm256_loadu_pd(v+vixd(hops[hixd(t,mu,1)],gi,gamx_pd[s]));
+//                     // because mu=0,1: swap the 2 numbers in the register
+//                     v_Hmup_gam = _mm256_permute4x64_pd(v_Hmup_gam,78);
+//                     v_Hmup_gam = compl_vectorreg_pointwise_mul(gamf2s,v_Hmup_gam);
+
+//                     // v hop in positive mu
+//                     __m256d v_Hmup = _mm256_loadu_pd(v+vixd(hops[hixd(t,mu,1)],gi,s));
+
+//                     // subtract those 2
+//                     v_Hmup = _mm256_sub_pd(v_Hmup, v_Hmup_gam);
+
+//                     // multiply U at this point onto v sum
+//                     v_Hmup = compl_scalarmem_vectorreg_mul(U+uixd(t,mu,g,gi),v_Hmup);
+
+
+//                     // add both U*v terms
+//                     v_Hmum = _mm256_add_pd(v_Hmum,v_Hmup);
+
+//                     // *(-0.5) and add to incr
+//                     incr = _mm256_fmadd_pd(v_Hmum,m0p5_reg,incr);
+
+//                 }
+//                 // store incr in result
+//                 _mm256_storeu_pd(result+vixd(t,g,s),incr);
+//             }
+//         }
+//         mu = 1;
+//         for (int g = 0; g < 3; g++){
+//             for (int s = 0; s < 4; s+=2){
+
+//                 // load result of previous computations to add to
+//                 __m256d incr = _mm256_loadu_pd(result+vixd(t,g,s));
+
+//                 // gamma_mu prefactor for the spins s and s+1
+//                 __m256d gamf2s = _mm256_loadu_pd(gamfd+gixd(mu,s));
+
+//                 for (int gi = 0; gi < 3; gi++){
+//                     // v hop in negative mu * gammma
+//                     __m256d v_Hmum_gam = _mm256_loadu_pd(v+vixd(hops[hixd(t,mu,0)],gi,gamx_pd[s]));
+//                     // because mu=0,1: swap the 2 numbers in the register
+//                     v_Hmum_gam = _mm256_permute4x64_pd(v_Hmum_gam,78);
+//                     // multiply the gamma prefactor for s and s+1
+//                     v_Hmum_gam = compl_vectorreg_pointwise_mul(gamf2s,v_Hmum_gam);
+
+//                     // v hop in negative mu
+//                     __m256d v_Hmum = _mm256_loadu_pd(v+vixd(hops[hixd(t,mu,0)],gi,s));
+
+//                     // add those together
+//                     v_Hmum = _mm256_add_pd(v_Hmum, v_Hmum_gam);
+
+//                     // take Umu hop in negative mu, adjoint it, and multiply onto v sum
+//                     v_Hmum = compl_scalarmem_conj_vectorreg_mul(U+uixd(hops[hixd(t,mu,0)],mu,gi,g),v_Hmum);
+
+
+//                     // v hop in positive mu * gamma
+//                     __m256d v_Hmup_gam = _mm256_loadu_pd(v+vixd(hops[hixd(t,mu,1)],gi,gamx_pd[s]));
+//                     // because mu=0,1: swap the 2 numbers in the register
+//                     v_Hmup_gam = _mm256_permute4x64_pd(v_Hmup_gam,78);
+//                     v_Hmup_gam = compl_vectorreg_pointwise_mul(gamf2s,v_Hmup_gam);
+
+//                     // v hop in positive mu
+//                     __m256d v_Hmup = _mm256_loadu_pd(v+vixd(hops[hixd(t,mu,1)],gi,s));
+
+//                     // subtract those 2
+//                     v_Hmup = _mm256_sub_pd(v_Hmup, v_Hmup_gam);
+
+//                     // multiply U at this point onto v sum
+//                     v_Hmup = compl_scalarmem_vectorreg_mul(U+uixd(t,mu,g,gi),v_Hmup);
+
+
+//                     // add both U*v terms
+//                     v_Hmum = _mm256_add_pd(v_Hmum,v_Hmup);
+
+//                     // *(-0.5) and add to incr
+//                     incr = _mm256_fmadd_pd(v_Hmum,m0p5_reg,incr);
+
+//                 }
+//                 // store incr in result
+//                 _mm256_storeu_pd(result+vixd(t,g,s),incr);
+//             }
+//         }
+//         for (mu = 2; mu < 4; mu++){
+//             for (int g = 0; g < 3; g++){
+//                 for (int s = 0; s < 4; s+=2){
+
+//                     // load result of previous computations to add to
+//                     __m256d incr = _mm256_loadu_pd(result+vixd(t,g,s));
+
+//                     // gamma_mu prefactor for the spins s and s+1
+//                     __m256d gamf2s = _mm256_loadu_pd(gamfd+gixd(mu,s));
+
+//                     for (int gi = 0; gi < 3; gi++){
+//                         // v hop in negative mu * gammma
+//                         __m256d v_Hmum_gam = _mm256_loadu_pd(v+vixd(hops[hixd(t,mu,0)],gi,gamx_pd[s]));
+//                         // multiply the gamma prefactor for s and s+1
+//                         v_Hmum_gam = compl_vectorreg_pointwise_mul(gamf2s,v_Hmum_gam);
+
+//                         // v hop in negative mu
+//                         __m256d v_Hmum = _mm256_loadu_pd(v+vixd(hops[hixd(t,mu,0)],gi,s));
+
+//                         // add those together
+//                         v_Hmum = _mm256_add_pd(v_Hmum, v_Hmum_gam);
+
+//                         // take Umu hop in negative mu, adjoint it, and multiply onto v sum
+//                         v_Hmum = compl_scalarmem_conj_vectorreg_mul(U+uixd(hops[hixd(t,mu,0)],mu,gi,g),v_Hmum);
+
+
+//                         // v hop in positive mu * gamma
+//                         __m256d v_Hmup_gam = _mm256_loadu_pd(v+vixd(hops[hixd(t,mu,1)],gi,gamx_pd[s]));
+//                         v_Hmup_gam = compl_vectorreg_pointwise_mul(gamf2s,v_Hmup_gam);
+
+//                         // v hop in positive mu
+//                         __m256d v_Hmup = _mm256_loadu_pd(v+vixd(hops[hixd(t,mu,1)],gi,s));
+
+//                         // subtract those 2
+//                         v_Hmup = _mm256_sub_pd(v_Hmup, v_Hmup_gam);
+
+//                         // multiply U at this point onto v sum
+//                         v_Hmup = compl_scalarmem_vectorreg_mul(U+uixd(t,mu,g,gi),v_Hmup);
+
+
+//                         // add both U*v terms
+//                         v_Hmum = _mm256_add_pd(v_Hmum,v_Hmup);
+
+//                         // *(-0.5) and add to incr
+//                         incr = _mm256_fmadd_pd(v_Hmum,m0p5_reg,incr);
+
+//                     }
+//                     // store incr in result
+//                     _mm256_storeu_pd(result+vixd(t,g,s),incr);
+//                 }
+//             }
+//         }
+//         //} end mu loop
+
+//     }
+
+
+//     return result_tensor;
+// }
+
 }
---
+++ test/test_12_bench_dirac_simd_size.py
@@ -0,0 +1,161 @@
+import torch
+import socket
+import numpy as np
+import time
+import pytest
+import copy
+
+import qcd_ml_accel_dirac
+import qcd_ml
+
+# This test is to generate test data for multiple different lattice sizes
+
+try:
+    import gpt as g # type: ignore
+    import qcd_ml_accel_dirac.compat
+
+    def test_throughput_wilson_different_volumes():
+        print()
+        num_threads = torch.get_num_threads()
+        print("running on host", socket.gethostname())
+        print(f'Machine has {num_threads} threads')
+
+        lat_dims = [[4,4,4,4]]
+        for li in range(9):
+            newdim = copy.copy(lat_dims[li])
+            newdim[(li+3)%4] *= 2
+            lat_dims.append(newdim)
+        
+        print("lattice_dimensions_used = ", lat_dims)
+
+        mass = -0.5
+        print("mass parameter =",mass)
+
+        check_correct = []
+
+        rng = g.random("sizetest")
+
+        meantimes = {"avx":[], "gpt":[]}
+        timestdevs = {"avx":[], "gpt":[]}
+        meanthroughputs = {"avx":[], "gpt":[]}
+        thrptstdevs = {"avx":[], "gpt":[]}
+
+        for lat_dim in lat_dims:
+            n_measurements = 1000
+            n_warmup = 10
+
+            #print("=====")
+            print("testing lattice dimensions",lat_dim)
+
+            U_g = g.qcd.gauge.random(g.grid(lat_dim, g.double), rng)
+            grid = U_g[0].grid
+            v_g = rng.cnormal(g.vspincolor(grid))
+            dst_g = g.lattice(v_g)
+            
+            kappa = 1.0/2.0/(mass + 4.0)
+
+            dw_g = g.qcd.fermion.wilson_clover(U_g, {"kappa":kappa,"csw_r":0.0,"csw_t":0.0,"xi_0":1,"nu":1,
+                                                "isAnisotropic":False,"boundary_phases":[1.0,1.0,1.0,1.0],}, )
+
+            U = torch.tensor(qcd_ml_accel_dirac.compat.lattice_to_array(U_g))
+            v = torch.tensor(qcd_ml_accel_dirac.compat.lattice_to_array(v_g))
+
+            Unew = torch.permute(U,(1,2,3,4,0,5,6)).contiguous()
+            vnew = torch.permute(v,(0,1,2,3,5,4)).contiguous()
+            
+            dw_py = qcd_ml.qcd.dirac.dirac_wilson(U,mass)
+            dwv_py = dw_py(v)
+
+            dw_avx = qcd_ml_accel_dirac.dirac_wilson_avx(Unew,mass)
+
+            dst_g = dw_g(v_g)
+            dwv_avx = dw_avx(vnew)
+
+            for _ in range(n_warmup):
+                dst_g = dw_g(v_g)
+                dwv_avx = dw_avx(vnew)
+
+            results_avx = np.zeros(n_measurements)
+            results_g = np.zeros(n_measurements)
+            bias = np.zeros(n_measurements)
+
+            for i in range(n_measurements):
+                start = time.perf_counter_ns()
+                dwv_avx = dw_avx(vnew)
+                stop = time.perf_counter_ns()
+                results_avx[i] = stop - start
+
+                start = time.perf_counter_ns()
+                dst_g = dw_g(v_g)
+                stop = time.perf_counter_ns()
+                results_g[i] = stop - start
+
+                start = time.perf_counter_ns()
+                stop = time.perf_counter_ns()
+                bias[i] = stop - start
+
+
+            results_avx_sorted = np.sort(results_avx)[:(n_measurements // 5)]
+            results_g_sorted = np.sort(results_g)[:(n_measurements // 5)]
+            
+            # size of the additional hop term lookup table (only for dw_avx)
+            hop_size = dw_avx.hop_inds.element_size() * dw_avx.hop_inds.nelement()
+
+            for pack,results_sorted,h in [["gpt", results_g_sorted, 0],
+                                          ["avx", results_avx_sorted, hop_size]]:
+                
+                data_size = v.element_size() * v.nelement() * 2 + U.element_size() * U.nelement() + h
+                data_size_MiB = data_size / 1024**2
+
+                throughput = data_size / (results_sorted / 1000**3)
+                throughput_GiBs = throughput / 1024 ** 3
+
+                throughput_peak = data_size / (results_sorted[0] / 1000**3)
+                throughput_peak_GiBs = throughput_peak / 1024 ** 3
+
+                # print("-----")
+                # print(pack)
+                # print(f"mean (top 20%): [us] {np.mean(results_sorted)/1000: .2f}")
+                # print(f"std (top 20%): [us] {np.std(results_sorted)/1000: .2f}")
+                # print(f"best : [us] {results_sorted[0]/1000}")
+                # print(f"mean bias : [us] {np.mean(bias)/1000}")
+                # print(f"std bias : [us] {np.mean(bias)/1000}")
+
+                # print()
+                # print(f"data : [MiB] {data_size_MiB: .3f}")
+
+                # print(f"throughput : [GiB/s] {np.mean(throughput_GiBs): .3f}")
+                # print(f"std : [GiB/s] {np.std(throughput_GiBs)}")
+                # print(f"peak thrpt. : [GiB/s] {throughput_peak_GiBs: .3f}")
+
+
+                meantimes[pack].append(np.mean(results_sorted) / 1000)
+                timestdevs[pack].append((np.std(results_sorted) + np.mean(bias)) / 1000)
+                meanthroughputs[pack].append(np.mean(throughput_GiBs))
+                thrptstdevs[pack].append(np.std(throughput_GiBs))
+    
+                
+            dst_torch = torch.tensor(qcd_ml_accel_dirac.compat.lattice_to_array(dst_g))
+            dwv_avx_old = torch.permute(dwv_avx, (0,1,2,3,5,4))
+            check_correct.append(torch.allclose(dwv_py,dwv_avx_old))
+            check_correct.append(torch.allclose(dwv_py,dst_torch))
+
+        print("avx_time_means_in_us =", meantimes["avx"])
+        print("gpt_time_means_in_us =", meantimes["gpt"])
+        print("avx_time_errors_in_us =", timestdevs["avx"])
+        print("gpt_time_errors_in_us =", timestdevs["gpt"])
+        print("avx_throughput_means_in_GiB_per_s =", meanthroughputs["avx"])
+        print("gpt_throughput_means_in_GiB_per_s =", meanthroughputs["gpt"])
+        print("avx_throughput_errors_in_GiB_per_s =", thrptstdevs["avx"])
+        print("gpt_throughput_errors_in_GiB_per_s =", thrptstdevs["gpt"])
+
+        assert all(check_correct)
+    
+
+except ImportError:
+
+    @pytest.mark.skip("missing gpt")
+    def test_throughput_wilson_different_volumes():
+        pass
+
+
